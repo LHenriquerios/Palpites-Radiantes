d1e9e1872358bfa112f08188ea917f36c571f610
LHenriquerios <henriquerios4@gmail.com>
2023-03-14 17:26:15 -0300

    feat: create migrations

---
+++ backend/src/database/migrations/20230314201920-create-matches.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idTeam1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'teams', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      idteam2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'teams', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      victoriesTeam1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      victoriesTeam2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
