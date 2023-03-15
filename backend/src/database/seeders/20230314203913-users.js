eb7de5a7c7995577451301966bccae6895057eb1
LHenriquerios <henriquerios4@gmail.com>
2023-03-15 07:57:40 -0300

    feat: add email and password fieds

--- backend/src/database/seeders/20230314203913-users.js
+++ backend/src/database/seeders/20230314203913-users.js
@@ -5,15 +5,21 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
       {
-        name: 'Guneko',
+        username: 'Guneko',
+        email: 'guneko@example.com',
+        password: '123456',
         points: 10,
       },
       {
-        name: 'LaneBoy',
+        username: 'LaneBoy',
+        email: 'laneboy@example.com',
+        password: '123456',
         points: 20,
       },
       {
         name: 'Draft5',
+        email: 'draft5@example.com',
+        password: '123456',
         points: 5,
       },
     ]);
