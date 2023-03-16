7273e61a957703109920b882e3fef5d1b42a74dc
LHenriquerios <henriquerios4@gmail.com>
2023-03-16 13:51:46 -0300

    fix: error

--- backend/src/controllers/team.js
+++ backend/src/controllers/team.js
@@ -1,29 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
-const teamService = require('../services/team');
+const teamServices = require('../services/team');
 
-// não precisa do next e try catch por conta do rescue
-exports.createTeam = async (req, res) => {
-  await teamService.createTeam(req.body);
-  res.status(StatusCodes.CREATED).json({ message: 'Nova equipe cadastrada' });
+// não precisa do next por conta do rescue
+const getAll = async (_req, res) => {
+  const teams = await teamServices.getAll();
+  return res.status(StatusCodes.OK).json(teams);
 };
 
-exports.getAllTeams = async (_req, res) => {
-  console.log('tá no controller');
-  const teams = await teamService.getAllTeams();
-  res.status(StatusCodes.OK).json(teams);
-};
-
-exports.getTeamById = async (req, res) => {
-  const team = await teamService.getTeamById(req.params.id);
-  res.status(StatusCodes.OK).json(team);
-};
-
-exports.updateTeam = async (req, res) => {
-  const team = await teamService.updateTeam(req.params.id, req.body);
-  res.status(StatusCodes.OK).json(team);
-};
-
-exports.deleteTeam = async (req, res) => {
-  await teamService.deleteTeam(req.params.id);
-  res.status(StatusCodes.NO_CONTENT).json({ success: true });
+module.exports = {
+  getAll,
 };
--- backend/src/routes/teamRoute.js
+++ backend/src/routes/teamRoute.js
@@ -3,10 +3,6 @@ const rescue = require('express-rescue');
 const teamController = require('../controllers/team');
 const teamRouter = express.Router();
 
-teamRouter.post('/', rescue(teamController.createTeam));
-teamRouter.get('/', rescue(teamController.getAllTeams));
-teamRouter.get('/:id', rescue(teamController.getTeamById));
-teamRouter.put('/:id',  rescue(teamController.updateTeam));
-teamRouter.delete('/:id', rescue(teamController.deleteTeam));
+teamRouter.get('/', rescue(teamController.getAll));
 
 module.exports = teamRouter;
